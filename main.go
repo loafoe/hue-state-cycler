a48d023a398aa7c9ba18d660a948df89445541e5
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-07-10 14:25:42 +0200

    Fix typo

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	// Server
 	e := echo.New()
 	e.GET("/status", func(c echo.Context) error {
-		return e.JSON(http.StatusOK, jsonOK(c, "UP"))
+		return c.JSON(http.StatusOK, jsonOK(c, "UP"))
 	})
 	e.POST("/cycle/:deviceID", cycleHandler(bridge))
 	_ = e.Start(":8080")
